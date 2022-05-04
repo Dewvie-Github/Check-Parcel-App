9a3cba8ff1ccb06ad1a7591bdb7f60dab0be4088
Saharat Thongin <6306021620191@fitm.kmutnb.ac.th>
2022-05-04 12:36:57 +0700

    update back to docker

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// docker
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -12,7 +13,8 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3000,
+    port: 80,
+    
   },
   resolve: {
     alias: {
